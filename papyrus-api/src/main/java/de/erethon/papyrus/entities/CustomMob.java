5dc4c3fcc3eed0d0b250e59c8e4909c81a32ee40
Malfrador <malfrador@gmail.com>
2025-01-26 19:25:53 +0100

    CustomMob should extend Mob obviously

--- papyrus-api/src/main/java/de/erethon/papyrus/entities/CustomMob.java
+++ papyrus-api/src/main/java/de/erethon/papyrus/entities/CustomMob.java
@@ -1,8 +1,9 @@
 package de.erethon.papyrus.entities;
 
 import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Mob;
 
-public interface CustomMob {
+public interface CustomMob extends Mob {
 
     void setType(EntityType type);
     String getPapyrusId();
