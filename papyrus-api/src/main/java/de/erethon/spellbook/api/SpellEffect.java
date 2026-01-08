bf5f7ad093bd65d0281131b8b0747af2b477fa0b
malfrador <malfrador@gmail.com>
2026-01-08 05:53:12 +0100

    Change effect stacking logic

--- papyrus-api/src/main/java/de/erethon/spellbook/api/SpellEffect.java
+++ papyrus-api/src/main/java/de/erethon/spellbook/api/SpellEffect.java
@@ -3,6 +3,10 @@ package de.erethon.spellbook.api;
 import de.erethon.papyrus.PDamageType;
 import org.bukkit.entity.LivingEntity;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class SpellEffect {
 
     protected LivingEntity target;
@@ -11,53 +15,84 @@ public class SpellEffect {
 
     protected int ticksLeft;
     protected int stacks;
+    protected final List<Integer> stackDurations = new ArrayList<>();
 
     public SpellEffect(EffectData data, LivingEntity caster, LivingEntity target, int duration, int stacks) {
         this.data = data;
         this.caster = caster;
         this.target = target;
-        this.ticksLeft = duration;
         this.stacks = stacks;
+
+        for (int i = 0; i < stacks; i++) {
+            this.stackDurations.add(duration);
+        }
+        this.ticksLeft = duration;
     }
 
     public void tick() {
-        ticksLeft--;
+        if (!stackDurations.isEmpty()) {
+            stackDurations.replaceAll(t -> t - 1);
+            stackDurations.removeIf(t -> t <= 0);
+            this.stacks = stackDurations.size();
+            this.ticksLeft = stackDurations.isEmpty() ? 0 : Collections.max(stackDurations);
+        } else {
+            ticksLeft--;
+            stacks = 0;
+        }
+
         onTick();
     }
 
-    public void add(int duration, int stacks) {
+    public void add(int duration, int stacksToAdd) {
         if (data.getStackMode() == EffectData.StackMode.PROLONG) {
-            this.ticksLeft += duration;
+            stackDurations.replaceAll(t -> t + duration);
+            if (stackDurations.isEmpty()) {
+                stackDurations.add(duration);
+            }
+
         } else if (data.getStackMode() == EffectData.StackMode.INTENSIFY) {
-            this.stacks += stacks;
+            for (int i = 0; i < stacksToAdd; i++) {
+                if (this.stackDurations.size() < data.getMaxStacks()) {
+                    this.stackDurations.add(duration);
+                }
+            }
         }
+
+        this.stacks = stackDurations.size();
+        this.ticksLeft = stackDurations.isEmpty() ? 0 : Collections.max(stackDurations);
+
         onApply();
     }
 
     public boolean canAdd(int duration, int newStacks) {
-        if (data.getStackMode() == EffectData.StackMode.PROLONG) {
-            return ticksLeft + duration <= data.getMaxDuration();
-        } else if (data.getStackMode() == EffectData.StackMode.INTENSIFY) {
-            return stacks + newStacks <= data.getMaxStacks();
+        if (data.getStackMode() == EffectData.StackMode.INTENSIFY) {
+            return this.stacks + newStacks <= data.getMaxStacks();
         }
-        // Default: check both (or return false for unknown modes)
-        return ticksLeft + duration <= data.getMaxDuration() && stacks + newStacks <= data.getMaxStacks();
+        if (data.getStackMode() == EffectData.StackMode.PROLONG) {
+            return this.ticksLeft + duration <= data.getMaxDuration();
+        }
+
+        return true;
+    }
+
+    public boolean shouldRemove() {
+        return stackDurations.isEmpty() || ticksLeft <= 0;
     }
 
     public void onTick() {
     }
 
-    public void onApply() { // Mostly useful for stat buffs/debuffs
+    public void onApply() {
     }
 
-    public void onRemove() { // Mostly useful for stat buffs/debuffs
+    public void onRemove() {
     }
 
-    public double onDamage(LivingEntity damager, double damage, PDamageType type) { // For effects that block 1 attack and get removed after that
+    public double onDamage(LivingEntity damager, double damage, PDamageType type) {
         return damage;
     }
 
-    public double onAttack(LivingEntity target, double damage, PDamageType type) { // For effects that add something to the attack
+    public double onAttack(LivingEntity target, double damage, PDamageType type) {
         return damage;
     }
 
@@ -77,24 +112,37 @@ public class SpellEffect {
         return true;
     }
 
-    public boolean shouldRemove() {
-        return ticksLeft <= 0;
-    }
-
     public int getStacks() {
         return stacks;
     }
 
     public void setStacks(int stacks) {
-        this.stacks = stacks;
+
+        int diff = stacks - this.stackDurations.size();
+
+        if (diff > 0) {
+            int duration = this.ticksLeft > 0 ? this.ticksLeft : 20; // Default or current
+            for (int i = 0; i < diff; i++) {
+                this.stackDurations.add(duration);
+            }
+        } else if (diff < 0) {
+            for (int i = 0; i < Math.abs(diff); i++) {
+                if (!stackDurations.isEmpty()) {
+                    Integer min = Collections.min(stackDurations);
+                    stackDurations.remove(min);
+                }
+            }
+        }
+        this.stacks = stackDurations.size();
     }
 
     public int getTicksLeft() {
-        return ticksLeft;
+        return ticksLeft; // Returns the duration of the stack that lasts the longest
     }
 
     public void setTicksLeft(int ticksLeft) {
         this.ticksLeft = ticksLeft;
+        this.stackDurations.replaceAll(t -> ticksLeft);
     }
 
     public LivingEntity getCaster() {
